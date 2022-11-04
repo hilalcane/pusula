385cc4e9bbad5f4f481c694b5e68bd42b545937a
hilalcane <2170656020@nku.edu.tr>
2022-11-04 16:24:53 +0300

    pages created

---
+++ src/app/features/authentication/authentication.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const authenticationRoutes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+  }
+];
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(authenticationRoutes)
+  ]
+})
+export class AuthenticationModule { }
